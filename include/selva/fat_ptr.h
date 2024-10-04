76aaa234f09166990e95b3df7aa371b5ae450c6c
Eva Pace <eba.pachi@gmail.com>
2024-10-04 00:18:13 -0300

    uhm, let's actually use memcpy instead of = (more flexible)

--- include/selva/fat_ptr.h
+++ include/selva/fat_ptr.h
@@ -3,6 +3,7 @@
 #ifdef DEBUG
 #include <assert.h>
 #endif
+#include <memory.h>
 
 #include "primitives.h"
 
@@ -16,7 +17,7 @@
 #ifdef DEBUG
 #define fat_ptr_push(ptr, item)                                   \
     assert(ptr->len <= sizeof(ptr->data) / sizeof(ptr->data[0])); \
-    ptr->data[ptr->len++] = item
+    memcpy(ptr->data + ptr->len++, item, sizeof(ptr->data[0]))
 #else
 #define fat_ptr_push(ptr, item) \
     ptr->data[ptr->len++] = item
